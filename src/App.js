299b5b91c08bc00f1f20ec44002351a5573e1e11
Sean Ray <sean.ray@wwt.com>
2019-02-09 18:29:37 -0500

    Show which menu item is selected, and small tweaks
    - Highlight selected left nav item
    - Make github icon link larger
    - Fix remaining column width when left nav hidden
    - Fix typo on home page

--- src/App.js
+++ src/App.js
@@ -6,45 +6,59 @@ import Weather from './Weather';
 import Calculator from './Calculator';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      activeLink: null
+    };
+  }
+  
+  handleLinkClick(linkName) {
+    this.setState({activeLink: linkName});
+  }
+  
   render() {
     return (
       <Router>
       <div>
         
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link className="navbar-brand" to="/react-playground">React Playground</Link>
+          <Link className="navbar-brand" onClick={() => this.handleLinkClick()} to="/react-playground">React Playground</Link>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item dropdown d-lg-none">
-              <button className="btn nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <button className="btn nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Projects
               </button>
               <div className="dropdown-menu position-absolute" aria-labelledby="navbarDropdown">
-                <Link className="dropdown-item" to="/react-playground/weather">Weather</Link>
-                <Link className="dropdown-item" to="/react-playground/calculator">Calculator</Link>
+                <Link className="dropdown-item" onClick={() => this.handleLinkClick('weather')} to="/react-playground/weather">Weather</Link>
+                <Link className="dropdown-item" onClick={() => this.handleLinkClick('calculator')} to="/react-playground/calculator">Calculator</Link>
               </div>
             </li>
           </ul>
           
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a href="https://github.com/skray/react-playground" className="nav-link"><i className="fab fa-github"></i></a>
+              <a href="https://github.com/skray/react-playground" className="nav-link">
+                <i className="fab fa-github fa-lg"></i>
+              </a>
             </li>
           </ul>
         </nav>
         
-        <div className="container-fluid">
-          <div className="row">
+        <div className="container-fluid p-0">
+          <div className="row  no-gutters">
             <div className="col-2 border-right min-vh-100 d-none d-lg-block">
-              <ul className="nav flex-column mt-3">
+              <ul className="nav flex-column mt-3 list-group list-group-flush">
                 <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/react-playground/weather">Weather</Link>
+                  <Link className={`list-group-item list-group-item-action ${this.state.activeLink === 'weather' ? 'bg-secondary text-light' : ''}`} onClick={() => this.handleLinkClick('weather')} to="/react-playground/weather">Weather</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/react-playground/calculator">Calculator</Link>
+                  <Link className={`list-group-item list-group-item-action ${this.state.activeLink === 'calculator' ? 'bg-secondary text-light' : ''}`} onClick={() => this.handleLinkClick('calculator')} to="/react-playground/calculator">Calculator</Link>
                 </li>
               </ul>
             </div>
-            <div className="col-10">
+            <div className="col col-lg-10 p-3">
               <Route path="/react-playground" exact component={Home} />
               <Route path="/react-playground/weather" component={Weather} />
               <Route path="/react-playground/calculator" component={Calculator} />
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default () => (
   <div className="container mt-3">
     <div className="row">
       <div className="col">
-        <h1>Welcome to my React Plaground</h1>
+        <h1>React Playground</h1>
         <p>Click the links on the left to explore</p>
       </div>
     </div>
