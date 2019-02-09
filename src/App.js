36bb94070e6257e45aebab88b4fa88c7358f583b
Sean Ray <sean.ray@wwt.com>
2019-02-08 21:23:28 -0500

    Fixing router to use full paths

--- src/App.js
+++ src/App.js
@@ -12,24 +12,24 @@ class App extends Component {
       <Router>
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link className="navbar-brand" to="/">React Playground</Link>
+          <Link className="navbar-brand" to="/react-playground">React Playground</Link>
         </nav>
         <div className="container-fluid">
           <div className="row">
             <div className="col-2 bg-light min-vh-100">
               <ul className="nav flex-column mt-3">
                 <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/weather">Weather</Link>
+                  <Link className="nav-link active text-dark" to="/react-playground/weather">Weather</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/calculator">Calculator</Link>
+                  <Link className="nav-link active text-dark" to="/react-playground/calculator">Calculator</Link>
                 </li>
               </ul>
             </div>
             <div className="col-10">
-              <Route path="/" exact component={Home} />
-              <Route path="/weather" component={Weather} />
-              <Route path="/calculator" component={Calculator} />
+              <Route path="/react-playground" exact component={Home} />
+              <Route path="/react-playground/weather" component={Weather} />
+              <Route path="/react-playground/calculator" component={Calculator} />
             </div>
           </div>
         </div>
